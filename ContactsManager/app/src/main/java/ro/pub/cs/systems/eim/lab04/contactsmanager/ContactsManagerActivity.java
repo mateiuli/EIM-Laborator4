bce3db0f061ebb43f8a4556f1ab388d6a0d82fc1
Student User <student@cti.pub.ro>
2017-03-21 08:49:57 +0200

    Implememts show/hide for additional container.

--- ContactsManager/app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/ContactsManagerActivity.java
+++ ContactsManager/app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/ContactsManagerActivity.java
@@ -2,12 +2,35 @@ package ro.pub.cs.systems.eim.lab04.contactsmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class ContactsManagerActivity extends AppCompatActivity {
+public class ContactsManagerActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_manager);
+
+        Button hide_show_button = (Button) findViewById(R.id.hide_show_btn);
+        hide_show_button.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view) {
+        if(view instanceof Button) {
+            Button hide_show_button = (Button) view;
+            LinearLayout additional_container = (LinearLayout) findViewById(R.id.additional_container);
+            String btn_text = hide_show_button.getText().toString().toLowerCase();
+            if(btn_text.contains("show")) {
+                btn_text = "HIDE ADDITIONAL FIELDS";
+                additional_container.setVisibility(LinearLayout.VISIBLE);
+            } else if(btn_text.contains("hide")) {
+                btn_text = "SHOW ADDITIONAL FIELDS";
+                additional_container.setVisibility(LinearLayout.INVISIBLE);
+            }
+            hide_show_button.setText(btn_text);
+        }
     }
 }
